beedf9b605228938a67e3069fa19d25ab16b5527
Olga Lozovik <lozovikolga@gmail.com>
2016-10-09 18:33:51 +0300

    3 lab

---
+++ Lab3 — копия/Lab3/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Lab3 : Form
+    {
+        private int block;
+        private int takts = 1000000;
+        private int allRequestsCount;
+        private int requestOut;
+
+        private int requestsInQueueCount;
+        private int allRequestsInQueuetime;
+        private int singleRequestsInQueuetime;
+
+
+
+        public Lab3()
+        {
+            InitializeComponent();
+        }
+
+        private void SimulateBtn_Click(object sender, EventArgs e)
+        {
+            var source = new Source(double.Parse(textBox1.Text));
+            var firstChannel = new FirstChannel(double.Parse(textBox2.Text));
+            var secondChannel = new SecondChannel(double.Parse(textBox3.Text));
+            var queue = new Queue();
+            block = 0;
+            requestOut = 0;
+            allRequestsCount = 0;
+            requestsInQueueCount = 0;
+            allRequestsInQueuetime = 0;
+            singleRequestsInQueuetime = 0;
+
+            for (int i = 0; i < takts; i++)
+            {
+                var isSourceGenerate = source.GenerateRequest();
+
+                var isSecondChannelProcess = secondChannel.ProcessRequest();
+                var isFirstChannelProcess = firstChannel.ProcessRequest();
+
+
+                if (isSourceGenerate)
+                {
+                    allRequestsCount++;
+                }
+
+                if (isFirstChannelProcess && !secondChannel.HasRequest)
+                {
+                    secondChannel.HasRequest = true;
+                }
+                else if (isFirstChannelProcess && secondChannel.HasRequest)
+                {
+                    requestOut++;
+                    //сброс
+                }
+
+                if (isFirstChannelProcess && queue.HasRequest)
+                {
+                    queue.HasRequest = false;
+                    allRequestsInQueuetime += singleRequestsInQueuetime;
+                    singleRequestsInQueuetime = 0;
+                    firstChannel.HasRequest = true;
+                }
+
+                if (queue.HasRequest)
+                {
+                    singleRequestsInQueuetime++;
+                }
+
+                if (isSourceGenerate && !queue.HasRequest && !firstChannel.HasRequest)
+                {
+                    firstChannel.HasRequest = true;
+                }
+                else if (isSourceGenerate && !queue.HasRequest && firstChannel.HasRequest)
+                {
+                    queue.HasRequest = true;
+                    requestsInQueueCount++;
+                    singleRequestsInQueuetime++;
+                    source.IsBlocked = true;
+                }
+
+               /* else if (isSourceGenerate && queue.HasRequest)
+                {
+                    source.IsBlocked = true;
+                }*/
+
+
+
+                //unlock source
+                if (!queue.HasRequest && source.IsBlocked)
+                {
+                    source.IsBlocked = false;
+                }
+
+                if (source.IsBlocked)
+                {
+                    block++;
+                }
+            }
+
+            label5.Text = Math.Round((double)block/takts, 6).ToString();
+            label7.Text = Math.Round((double)(allRequestsCount - requestOut)/allRequestsCount, 6).ToString();
+            label9.Text = ((double) allRequestsInQueuetime/requestsInQueueCount).ToString();
+        }
+    }
+}
---
+++ Lab3/Lab3/Source.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab3
+{
+    public class Source
+    {
+        private readonly double probability;
+
+        public Source(double probability)
+        {
+            this.probability = probability;
+        }
+
+        public bool IsBlocked { get; set; } = false;
+
+        public bool GenerateRequest()
+        {
+            var random = new Random();
+            double val = random.NextDouble();
+
+            if (val > probability && !IsBlocked)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
